9ad079152d90be4793e827f2bc6884c778930d44
Ekpei <iekpei@yahoo.com>
2023-08-07 11:07:13 +0100

    javascript

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,17 @@
+guillaume@ubuntu:~/0x15$ cat 100-main.html 
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Holberton School</title>
+    <script type="text/javascript" src="100-script.js"></script>
+  </head>
+  <body>
+    <header> 
+      First HTML page
+    </header>
+    <footer>
+      Holberton School - 2017
+    </footer>
+  </body>
+</html>
+guillaume@ubuntu:~/0x15$ 
